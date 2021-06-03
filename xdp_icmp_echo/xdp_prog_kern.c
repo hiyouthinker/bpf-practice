241c7b12b856dcd8bd83fbd1e835ed8ab7f3602e
hiyouthinker <hi.youthinker@gmail.com>
2021-06-03 17:57:41 +0000

    calc checksum

--- xdp_icmp_echo/xdp_prog_kern.c
+++ xdp_icmp_echo/xdp_prog_kern.c
@@ -40,6 +40,38 @@ __u32 xdp_stats_record_action(struct xdp_md *ctx, __u32 action)
 	return action;
 }
 
+/*
+ * from xdp_tutorial/packet-solutions/xdp_prog_kern_03.c
+ */
+static __always_inline __u16 csum_fold_helper(__u32 csum)
+{
+	__u32 sum;
+	sum = (csum >> 16) + (csum & 0xffff);
+	sum += (sum >> 16);
+	return ~sum;
+}
+
+/*
+ * The icmp_checksum_diff function takes pointers to old and new structures and
+ * the old checksum and returns the new checksum.  It uses the bpf_csum_diff
+ * helper to compute the checksum difference. Note that the sizes passed to the
+ * bpf_csum_diff helper should be multiples of 4, as it operates on 32-bit
+ * words.
+ */
+static __always_inline __u16 icmp_checksum_diff(
+		__u16 seed,
+		struct icmphdr_common *icmphdr_new,
+		struct icmphdr_common *icmphdr_old)
+{
+#ifdef SUPPORT_BPF_CSUM
+	__u32 csum, size = sizeof(struct icmphdr_common);
+	csum = bpf_csum_diff((__be32 *)icmphdr_old, size, (__be32 *)icmphdr_new, size, seed);
+#else
+	__u32 csum = 0;
+#endif
+	return csum_fold_helper(csum);
+}
+
 SEC("xdp_icmp_echo")
 int xdp_icmp_echo_func(struct xdp_md *ctx)
 {
@@ -52,8 +84,9 @@ int xdp_icmp_echo_func(struct xdp_md *ctx)
 	int icmp_type;
 	struct iphdr *iphdr;
 	struct ipv6hdr *ipv6hdr;
-	__u16 echo_reply;
+	__u16 echo_reply, old_csum;
 	struct icmphdr_common *icmphdr;
+	struct icmphdr_common icmphdr_old;
 	__u32 action = XDP_PASS;
 
 	/* These keep track of the next header type and iterator pointer */
@@ -73,10 +106,16 @@ int xdp_icmp_echo_func(struct xdp_md *ctx)
 		goto out;
 	}
 
+	/*
+	 * We are using a special parser here which returns a stucture
+	 * containing the "protocol-independent" part of an ICMP or ICMPv6
+	 * header.  For purposes of this Assignment we are not interested in
+	 * the rest of the structure.
+	 */
 	icmp_type = parse_icmphdr_common(&nh, data_end, &icmphdr);
 	if (eth_type == bpf_htons(ETH_P_IP) && icmp_type == ICMP_ECHO) {
+		/* Swap IP source and destination */
 		swap_src_dst_ipv4(iphdr);
-		icmphdr->type = ICMP_ECHOREPLY;
 		echo_reply = ICMP_ECHOREPLY;
 	} else if (eth_type == bpf_htons(ETH_P_IPV6)
 		   && icmp_type == ICMPV6_ECHO_REQUEST) {
@@ -90,8 +129,34 @@ int xdp_icmp_echo_func(struct xdp_md *ctx)
 	/* Swap Ethernet source and destination */
 	swap_src_dst_mac(eth);
 
-	/* Assignment 1: patch the packet and update the checksum. You can use
-	 * the echo_reply variable defined above to fix the ICMP Type field. */
+
+	/* Patch the packet and update the checksum.*/
+	old_csum = icmphdr->cksum;
+	icmphdr->cksum = 0;
+	icmphdr_old = *icmphdr;
+	icmphdr->type = echo_reply;
+	icmphdr->cksum = icmp_checksum_diff(~old_csum, icmphdr, &icmphdr_old);
+
+	/* Another, less generic, but a bit more efficient way to update the
+	 * checksum is listed below.  As only one 16-bit word changed, the sum
+	 * can be patched using this formula: sum' = ~(~sum + ~m0 + m1), where
+	 * sum' is a new sum, sum is an old sum, m0 and m1 are the old and new
+	 * 16-bit words, correspondingly. In the formula above the + operation
+	 * is defined as the following function:
+	 *
+	 *     static __always_inline __u16 csum16_add(__u16 csum, __u16 addend)
+	 *     {
+	 *         csum += addend;
+	 *         return csum + (csum < addend);
+	 *     }
+	 *
+	 * So an alternative code to update the checksum might look like this:
+	 *
+	 *     __u16 m0 = * (__u16 *) icmphdr;
+	 *     icmphdr->type = echo_reply;
+	 *     __u16 m1 = * (__u16 *) icmphdr;
+	 *     icmphdr->checksum = ~(csum16_add(csum16_add(~icmphdr->checksum, ~m0), m1));
+	 */
 
 	action = XDP_TX;
 
